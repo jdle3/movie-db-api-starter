78bc7114cd95ecf0737e4f3b1ace958df412db43
jdle3 <jdle3@asu.edu>
2024-02-01 16:18:43 -0700

    birthyear

--- scripts.js
+++ scripts.js
@@ -8,7 +8,6 @@ function getPopularMovies(){
     const data = null;
 
     const xhr = new XMLHttpRequest();
-    // xhr.withCredentials = true;
 
     xhr.addEventListener('readystatechange', function () 
     {
@@ -34,13 +33,50 @@ function getPopularMovies(){
 
     xhr.open('GET', url);
     xhr.setRequestHeader('accept', 'application/json');
-    // xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNWRhYzQwMjllY2EyYjAwZDJmZjE5ZDNiYjYxNTZiMCIsInN1YiI6IjY1YmMxNDMyMmI4YTQzMDE2MTFiNGUwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.D1efkjD6yn_HssnDCw_3nZqIleWLixNka5OaT3YQons');
 
     xhr.send(data);
 }
 
 function getBirthYearMovies(){
+    e.preventDefault();
+    let year = encodeURI(document.getElementById("userYear"));
 
+    // to add key to the url
+    let url = `https://api.themoviedb.org/3/discover/movie?api_key=d5dac4029eca2b00d2ff19d3bb6156b0&primary_release_year=${year}&sort_by=revenue.desc&language=en-US&page=1&include_adult=false`;
+    let birthMovies = document.getElementById("birthYear");
+    let imgUrl = "https://image.tmdb.org/t/p/w500/";
+
+
+    const data = null;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('readystatechange', function () 
+    {
+        if (this.readyState === this.DONE) {
+            // console.log(this.responseText);
+
+            let json = JSON.parse(this.responseText);
+            console.log(json);
+
+            let html = "";
+            for(let i = 0; i < 4; i++){
+                html += `
+                <figure>
+                    <img src="${imgUrl}${json.results[i].poster_path}" alt="">
+                    <figcaption>${json.results[i].title}</figcaption>
+                </figure>
+                `;
+            }
+
+            birthMovies.innerHTML = html;
+        }
+    });
+
+    xhr.open('GET', url);
+    xhr.setRequestHeader('accept', 'application/json');
+
+    xhr.send(data);
 }
 
 window.addEventListener("load", function(){
